09978bafcdc8a5dc459684ed3412f80aa523f554
Samantha <roguegoatstudios@outlook.com>
2017-04-10 03:45:19 +0000

    checkpoint 23 progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   
   resources :topics do
- # #34
   resources :posts, except: [:index]
    end
 
-  get :action=>"show", :controller=>"advertisement"
+
   get 'about' => 'welcome#about'
-  
   root 'welcome#index'
 end

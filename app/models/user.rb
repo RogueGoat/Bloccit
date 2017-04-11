cfd3f773752975aa9384bf0e37e1d9b2eb41d52b
Samantha <roguegoatstudios@outlook.com>
2017-04-11 03:58:30 +0000

    checkpoint 24 assignment finished!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,29 +1,28 @@
 class User < ActiveRecord::Base
-   before_save { self.email = email.downcase if email.present? }
-   
-   before_save :reconfiure_name
-
+  before_save { self.email = email.downcase if email.present? }
+  before_save :configure_name
  # #3
-   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
- # #4
+  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+#  # #4
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
- # #5
-   validates :email,
+#  # #5
+  validates :email,
              presence: true,
              uniqueness: { case_sensitive: false },
              length: { minimum: 3, maximum: 254 }
 
- # #6
-   has_secure_password
-   
-   def reconfigure_name
-       if name
-           name_array = []
-           name.split.each do |name_segment|
-               name_array << name_segment.capitalize
-           end
-           
-           self.name = name_array.join(" ")
-   end
+#  # #6
+  has_secure_password
+  
+  def configure_name
+      if name
+          name_array = []
+          name.split.each do |name_segment|
+             name_array << name_segment.capitalize
+      end
+      self.name = name_array.join(" ")
+  end
+  end
+  
 end

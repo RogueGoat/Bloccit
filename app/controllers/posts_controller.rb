5d5c71655168bd70dd96847a0947d97c8dd580be
Samantha <roguegoatstudios@outlook.com>
2017-04-13 06:00:04 +0000

    checkpoint27 progress

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+    before_action :require_sign_in, except: :show
  
   def show
      @post = Post.find(params[:id])
@@ -11,12 +12,9 @@ class PostsController < ApplicationController
   
    def create
  # #9
-     @post = Post.new
-     @post.title = params[:post][:title]
-     @post.body = params[:post][:body]
+     @post = @topic.posts.build(post_params)
      @topic = Topic.find(params[:topic_id])
- # #35
-     @post.topic = @topic
+     @post.user = current_user
 
  # #10
      if @post.save
@@ -35,8 +33,7 @@ class PostsController < ApplicationController
   end
      def update
      @post = Post.find(params[:id])
-     @post.title = params[:post][:title]
-     @post.body = params[:post][:body]
+     @post.assign_attributes(post_params)
  
      if @post.save
        flash[:notice] = "Post was updated."
@@ -60,4 +57,9 @@ class PostsController < ApplicationController
      end
      end
 
+private
+ 
+   def post_params
+     params.require(:post).permit(:title, :body)
+   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,23 @@
    )
  end
  topics = Topic.all
+ 
+ 5.times do
+   User.create!(
+ # #3
+   name:     RandomData.random_name,
+   email:    RandomData.random_email,
+   password: RandomData.random_sentence
+   )
+ end
+ 
+ users = User.all
+ 
  # Create Posts
  50.times do
  # #1
    Post.create!(
+     user:   users.sample,
      topic:  topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
@@ -27,7 +40,14 @@
    )
  end
  
+ user = User.first
+ user.update_attributes!(
+   email: 'youremail.com', # replace this with your personal email
+   password: 'helloworld'
+ )
+ 
  puts "Seed finished"
+ puts "#{User.count} users created"
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
